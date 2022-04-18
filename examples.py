abfc01916731f0fbce967f8ef7a586cc6320862a
Keksiuwu <76953110+Keksiuwu@users.noreply.github.com>
2022-04-18 18:15:54 +0200

    Create examples.py

---
+++ examples.py
@@ -0,0 +1,16 @@
+import asyncio
+from kawaiiapi import Kawaii
+
+api = Kawaii("token")
+
+loop = asyncio.get_event_loop()
+
+print(loop.run_until_complete(api.endpoints("gif")))
+
+print(loop.run_until_complete(api.random("gif")))
+
+print(loop.run_until_complete(api.get("gif", "kiss")))
+
+print(loop.run_until_complete(api.gif("kiss")))
+
+print(loop.run_until_complete(api.stats()).all)
