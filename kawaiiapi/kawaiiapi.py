835e054594312770ec2c7571f84558a3671f48f8
Keksiuwu <76953110+keksiqc@users.noreply.github.com>
2022-06-22 13:36:25 +0200

    few code ajustments

--- kawaiiapi/kawaiiapi.py
+++ kawaiiapi/kawaiiapi.py
@@ -9,47 +9,50 @@ class Kawaii:
         self.token = token
         self.url = "https://kawaii.red/api/"
 
-    async def get(self, main: str, endpoint: str, f: Union[str, list] = []) -> str:
+    async def get(self, type: str, endpoint: str, f: Union[str, list] = []) -> str:
         async with aiohttp.ClientSession() as session:
-            async with session.get(f"{self.url}{main}/{endpoint}/token={self.token}&filter={f}/") as url:
+            async with session.get(f"{self.url}{type}/{endpoint}/token={self.token}&filter={f}/") as url:
                 image = await url.json()
-                return image["response"]
+                return image.get("response")
 
     async def text(self, text: str) -> str:
         async with aiohttp.ClientSession() as session:
             async with session.get(f"{self.url}txt/{text}/token={self.token}/") as url:
                 image = await url.json()
-                return image["response"]
+                return image.get("response")
 
     async def image(self, image: str) -> str:
         async with aiohttp.ClientSession() as session:
             async with session.get(f"{self.url}png/{image}/token={self.token}/") as url:
                 image = await url.json()
-                return image["response"]
+                return image.get("response")
 
     async def gif(self, gif: str) -> str:
         async with aiohttp.ClientSession() as session:
             async with session.get(f"{self.url}gif/{gif}/token={self.token}/") as url:
                 image = await url.json()
-                return image["response"]
+                return image.get("response")
 
-    async def random(self, main: str = "gif") -> str:
-        endpoint = random.choice(await self.endpoints(main))
+    async def random(self, type: str = "gif") -> str:
+        endpoint = random.choice(await self.endpoints(type))
         async with aiohttp.ClientSession() as session:
-            async with session.get(f"{self.url}{main}/{endpoint}/token={self.token}/") as url:
+            async with session.get(f"{self.url}{type}/{endpoint}/token={self.token}/") as url:
                 image = await url.json()
-                return image["response"]
+                return image.get("response")
 
-    async def endpoints(self, main: str="gif") -> list:
+    async def endpoints(self, type: str="gif") -> list:
         async with aiohttp.ClientSession() as session:
-            async with session.get(f"{self.url}{main}/endpoints/token={self.token}/") as url:
+            async with session.get(f"{self.url}{type}/endpoints/token={self.token}/") as url:
                 endpoints = await url.json()
-                return endpoints["response"]
+                return endpoints.get("response")
 
     async def stats(self) -> Stats:
+        if self.token == "anonymous":
+            raise Exception("You need to set a token to use this function")
+
         async with aiohttp.ClientSession() as session:
             async with session.get(f"{self.url}stats/endpoints/token={self.token}/") as url:
-                endpoints = (await url.json())["response"]
+                endpoints = (await url.json()).get("response")
 
             stats = {
                 "endpoints": endpoints
@@ -57,7 +60,7 @@ class Kawaii:
 
             for endpoint in endpoints:
                 async with session.get(f"{self.url}stats/{endpoint}/token={self.token}/") as url:
-                    stats[endpoint] = (await url.json())["response"]
+                    stats[endpoint] = (await url.json()).get("response")
             
             return Stats(**stats)
             
