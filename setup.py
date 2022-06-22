94698e8ecd6032f7c038f2d53cc813279b7b90b0
Keksiuwu <76953110+keksiqc@users.noreply.github.com>
2022-06-22 13:36:50 +0200

    new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='kawaiiapi',
-    version='1.2.2',
+    version='1.2.3',
     author='Keksiqc',
     author_email='contact@keksi.me',
     license='MIT',
